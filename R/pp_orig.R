907d443c418b257d4585ebd7a1a3c90028e4f9f5
Shu Xie <xieshu95@gmail.com>
2025-04-17 18:37:32 +0800

    original and temperature dependent pp model

---
+++ R/pp_orig.R
@@ -0,0 +1,76 @@
+library(tidyverse)
+library(deSolve)
+library(ggplot2)
+library(ggthemr)
+#' original predator-prey ODE without T and size dependence(type I)
+#' N is the biomass of prey and P is the biomass of predator
+#'
+predator_prey_type1 <- function(t, state, parameters) {
+  with(as.list(c(state, parameters)), {
+    dN <-  (1 - N/K)*r*N - a*N*P
+    dP <-  e*a*N*P - m*P
+    list(c(dN, dP))
+  })
+}
+parameters <- c(r = 0.3, a = 0.1, m = 0.4, e = 0.85, K = 20)
+state <- c(N = 2, P = 2)
+times <- seq(0, 200, by = 0.5)
+
+pp_results = as.data.frame(
+  ode(y = state, times = times, func = predator_prey_type1, parms = parameters)
+)
+
+
+pp_results_time = gather(pp_results, Species, Biomass, -time)
+
+# plot biomass N and P dynamics over time
+ggthemr('dust')
+ggplot(data = pp_results_time,aes(time, Biomass, colour = Species)) +
+  geom_line(size = 1)+
+  ggplot2::scale_colour_manual("Species",values = c("#E90F44","#63ADEE"))
+
+# plot N-P relationships over time
+ggthemr('dust')
+ggplot2::ggplot(pp_results) +
+  geom_path(aes(N, P, colour = time), size = 1)+
+  scale_color_gradientn(colours = rainbow(6))
+
+  # scale_color_gradient(low = "yellow", high = "red", na.value = NA)
+
+
+
+# lotka_volterra <- function(t, state, parameters) {
+#   with(as.list(c(state, parameters)), {
+#     dN <-  r*N - a*N*P
+#     dP <-  e*a*N*P - m*P
+#     list(c(dN, dP))
+#   })
+# }
+# parameters <- c(r = 0.3, a = 0.1, m = 0.4, e = 0.5)
+# state <- c(N = 2, P = 2)
+# times <- seq(0, 100, by = 0.2)
+# sol = as.data.frame(
+#   ode(y = state, times = times, func = lotka_volterra, parms = parameters)
+# )
+#
+# sol2 = gather(sol, species, individuals, -time)
+#
+# ggplot(sol2) +
+#   geom_line(aes(time, individuals, colour = species))+
+#   theme_minimal() +
+#   theme(
+#     plot.background = element_rect(fill = rgb(.2,.21,.27)),
+#     text = element_text(colour = 'grey'),
+#     axis.text = element_text(colour = 'grey'),
+#     panel.grid = element_line(colour = 'grey')
+#   )
+#
+# ggplot(sol) +
+#   geom_path(aes(N, P), colour = 'red', size = 1)+
+#   theme_minimal() +
+#   theme(
+#     plot.background = element_rect(fill = rgb(.2,.21,.27)),
+#     text = element_text(colour = 'grey'),
+#     axis.text = element_text(colour = 'grey'),
+#     panel.grid = element_line(colour = 'grey')
+#   )
---
+++ R/pp_tem.R
@@ -0,0 +1,48 @@
+library(tidyverse)
+library(deSolve)
+library(ggplot2)
+library(ggthemr)
+#' predator-prey ODE with T dependence but without size dependence(type I)
+#' N is the biomass of prey and P is the biomass of predator
+#'
+predator_prey_T <- function(t, state, parameters) {
+  with(as.list(c(state, parameters)), {
+    # update temperature
+    Temp <- T0 + n * t
+    # temperature-dependent rates
+    a <- a0 * exp(-Ea / (k * Temp))  # Arrhenius公式
+    m <- m0 * exp(-Em / (k * Temp))
+    r <- r0 * exp(-Er / (k * Temp))
+
+    dN <-  (1 - N/K)*r*N - a*N*P
+    dP <-  e*a*N*P - m*P
+    list(c(dN, dP),
+         # Temp = Temp,
+         r = r,
+         a = a,
+         m = m)
+  })
+}
+parameters <- c(T0 =300,k = 8.617*10^(-5),n = 0.02, a0 = 10^7, m0 = 10^6, r0 = 0.5*10^7, e = 0.85, K = 20, Ea = 0.6, Em = 0.5,Er = 0.6)
+state <- c(N = 2, P = 2)
+times <- seq(0, 10000, by = 1)
+
+pp_results = as.data.frame(
+  ode(y = state, times = times, func = predator_prey_T, parms = parameters)
+)
+
+
+pp_results_time = gather(pp_results, Species, Biomass, -time)
+
+ggthemr('dust')
+ggplot(data = pp_results_time,aes(time, Biomass, colour = Species)) +
+  geom_line(size = 1)+
+  ggplot2::scale_colour_manual("Species",values = c("#FFC839","#4daf4a","#E90F44","#63ADEE","#984ea3"))
+
+# plot N-P relationships over time
+ggthemr('dust')
+ggplot2::ggplot(pp_results) +
+  geom_path(aes(N, P, colour = time), size = 1)+
+  scale_color_gradientn(colours = rainbow(6))
+
+
